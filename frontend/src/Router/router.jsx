f8da1b918a757e832b8fd360f56e8655c7c967d8
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2025-03-22 14:56:38 +0600

    frontend  setup

---
+++ frontend/src/Router/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../home/Home";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      
+    ],
+  },
+]);
+
+export default router;
